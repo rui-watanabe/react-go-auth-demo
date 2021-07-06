a5c04f2c109b9b909733a40c4d88eb2b359ece16
rui-watanabe <wataru610806@gmail.com>
2021-07-06 17:57:08 +0900

    define users table

--- database/connection.go
+++ database/connection.go
@@ -1,15 +1,19 @@
 package database
 
 import (
+	"react-go-auth/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() {
 	dsn := "root@tcp(localhost:3306)/react-go-auth"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connection mysql")
 	}
+
+	connection.AutoMigrate(&models.User{})
 }
