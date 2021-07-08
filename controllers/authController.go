0c65032d77c0db0ff971ffe30ce71472c9663b66
rui-watanabe <wataru610806@gmail.com>
2021-07-08 20:03:04 +0900

    change passeord with bcrypt of user

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,11 +1,27 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"react-go-auth/models"
+
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
 	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+
+	user := models.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: password,
+	}
+
+	fmt.Println(user)
 	return c.JSON(data)
 }
--- models/user.go
+++ models/user.go
@@ -4,5 +4,5 @@ type User struct {
 	Id       int64
 	Name     string
 	Email    string `gorm:"unique"`
-	Password string
+	Password []byte
 }
